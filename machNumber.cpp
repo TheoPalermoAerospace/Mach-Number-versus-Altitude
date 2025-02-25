3b845ecc8a30c6cc587125cff528e48aab1d2d05
Theo Palermo <148143913+TheoPalermoAerospace@users.noreply.github.com>
2025-02-25 17:08:57 -0300

    Update machNumber.cpp
    much better

--- machNumber.cpp
+++ machNumber.cpp
@@ -7,6 +7,18 @@
 
 using namespace std;
 
+// Based on Atmosphere Temperature Distribuction
+
+struct AtmosphereParams {
+    double mTroposphere = -1 / 6.5;
+    double mStratosphere1 = 1;
+    double mStratosphere2 = 1 / 2.8;
+    double nTroposphere = 44.3;
+    double nStratosphere1 = -196.65;
+    double nStratosphere2 = -49.66;
+    double tropopauseTemperature = 216.65;
+};
+
 vector<double> linspace(double start, double end, int num) {
 	
 	vector<double> linearSpace; // Face coordinates vector
@@ -19,7 +31,6 @@ vector<double> linspace(double start, double end, int num) {
 
 	return linearSpace;
 }
-
 
 vector<double> calculateSoundSpeed(double gammaAir, double R, const vector<double>& temperatureVectors) {
 	
@@ -49,10 +60,12 @@ vector<double> calculateMachNumber(int velocity, const vector<double>& temperatu
 
 int main() {
 
-	double gammaAir = 1.4;
-	double R = 287.085;
+	const double gammaAir = 1.4;
+	const double R = 287.085;
 	int velocity = 315;
 	int h = 0;
+
+	AtmosphereParams params;
 
 	vector<double> temperatureVectors;
 	double temperatureTroposphere;
@@ -60,19 +73,9 @@ int main() {
 	double temperatureStratosphereSecondLayer;
 	double tropopauseTemperature = 216.65;
 
-	// Base on Atmosphere Temperature Distribuction
-
-	double m_troposphere = (-1)/6.5; // -11/(71.5)
-	double m_stratosphere_1 = 1; // (32-20)/(228.65-216.65) = 1
-	double m_stratosphere_2 = 1/2.8; // (47-32)/(270.65-228.65) = 15/42
-
-	double n_troposphere = 44.3;
-	double n_stratosphere_1 = -196.65;
-	double n_stratosphere_2 = -49.66;
-
 	do {
 		if (h >= 0 && h <= 11) {
-			temperatureTroposphere = (h-n_troposphere)/m_troposphere;
+			temperatureTroposphere = (h-params.nTroposphere)/params.mTroposphere;
 			temperatureVectors.push_back(temperatureTroposphere);
 			++h;
 		}
@@ -83,13 +86,13 @@ int main() {
 		}
 
 		else if (h >= 21 && h <= 32) {
-			temperatureStratosphereFirstLayer = (h-n_stratosphere_1)/m_stratosphere_1;
+			temperatureStratosphereFirstLayer = (h-params.nStratosphere1)/params.mStratosphere1;
 			temperatureVectors.push_back(temperatureStratosphereFirstLayer);
 			++h;
 		}
 
 		else {
-			temperatureStratosphereSecondLayer = (h-n_stratosphere_2)/m_stratosphere_2;
+			temperatureStratosphereSecondLayer = (h-params.nStratosphere2)/params.mStratosphere2;
 			temperatureVectors.push_back(temperatureStratosphereSecondLayer);
 			++h;
 		}
@@ -102,6 +105,12 @@ int main() {
 
 	// Save data to a file (data.txt)
     ofstream dataFile("data.txt");
+	
+	if (!dataFile) {
+	    cerr << "Error opening file for writing..." << endl;
+	    return -1;
+	}
+
     for (int i = 0; i < machNumberVector.size(); ++i) {
         dataFile << altitudeVector[i] << " " << machNumberVector[i] << endl;
     }
@@ -109,10 +118,6 @@ int main() {
 
     // Plotting using Gnuplot
 	system("gnuplot -e \"set terminal png; set output 'plot.png'; set grid; plot 'data.txt' using 2:1 with lines lc rgb 'black' lw 2 title 'Mach Number vs Altitude'\"");
-
-	/*for (int i = 0; i < machNumberVector.size(); ++i) {
-		cout << "Mach Number: " << machNumberVector[i] << endl;
-	}*/
 
 	return 0;
 }
